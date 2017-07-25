3184025584c2683bec0d578f85a8ef0696212913
bbbales2 <bbbales2@gmail.com>
2017-07-24 17:25:30 -0700

    Built some convergence checks

--- ising.cpp
+++ ising.cpp
@@ -47,6 +47,7 @@ List ising(NumericMatrix x, double mu, double beta, double kT, int S) {
   colnames(out) = CharacterVector::create("solo", "pairs");
   
   int N = x.nrow();
+  int accept = 0;
   
   std::random_device rd;
   std::mt19937 gen(rd());
@@ -54,6 +55,7 @@ List ising(NumericMatrix x, double mu, double beta, double kT, int S) {
   std::uniform_int_distribution<> dis(0, N - 1);
   double solo_ = solo(x);
   double pairs_ = pairs(x);
+  double E = mu * solo_ + beta * pairs_;
   for(int s = 0; s < S; s++) {
     int i = dis(gen);
     int j = dis(gen);
@@ -67,10 +69,11 @@ List ising(NumericMatrix x, double mu, double beta, double kT, int S) {
     double dE = mu * dsolo_ + beta * dpairs_;
     double r = runif(gen);
     
-    if(dE < 0.0 | std::exp(-dE / kT) > r) {
+    if(dE < 0.0 | r < std::exp(-dE / kT)) {
       solo_ = solo_ + dsolo_;
       pairs_ = pairs_ + dpairs_;
       x(i, j) = x(i, j) * -1;
+      accept += 1;
     }
     
     out(s, 0) = solo_;
@@ -80,10 +83,57 @@ List ising(NumericMatrix x, double mu, double beta, double kT, int S) {
   List ret;
   ret["x"] = x;
   ret["states"] = out;
+  ret["accept"] = accept / double(S);
   return ret;
 }
 
-
+//// [[Rcpp::export]]
+/*List ising_gibbs(NumericMatrix x, double mu, double beta, double kT, int S) {
+  NumericMatrix out(S, 2);
+  colnames(out) = CharacterVector::create("solo", "pairs");
+  
+  int N = x.nrow();
+  int accept = 0;
+  
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_real_distribution<> runif(0.0, 1.0);
+  std::uniform_int_distribution<> dis(0, N - 1);
+  double solo_ = solo(x);
+  double pairs_ = pairs(x);
+  double E = mu * solo_ + beta * pairs_;
+  for(int s = 0; s < S / (N * N); s++) {
+    for(int i = 0; i < N; i++) {
+      for(int j = 0; j < N; j++) {
+        double dsolo_ = -2 * x(i, j);
+        double dpairs_ = ((i < x.nrow() - 1) ? -2.0 * x(i, j) * x(i + 1, j) : 0) +
+          ((i > 0) ? -2.0 * x(i - 1, j) * x(i, j) : 0) +
+          ((j < x.ncol() - 1) ? -2.0 * x(i, j) * x(i, j + 1) : 0) +
+          ((j > 0) ? -2.0 * x(i, j - 1) * x(i, j) : 0);
+        
+        double dE = mu * dsolo_ + beta * dpairs_;
+        double r = runif(gen);
+        
+        if(dE < 0.0 | r < std::exp(-dE / kT)) {
+          solo_ = solo_ + dsolo_;
+          pairs_ = pairs_ + dpairs_;
+          E = mu * solo_ + beta * pairs_;
+          x(i, j) = x(i, j) * -1;
+          accept += 1;
+        }
+        
+        out(s, 0) = solo_;
+        out(s, 1) = pairs_;
+      }
+    }
+  }
+  
+  List ret;
+  ret["x"] = x;
+  ret["states"] = out;
+  ret["accept"] = accept / double(S);
+  return ret;
+}*/
 // You can include R code blocks in C++ files processed with sourceCpp
 // (useful for testing and development). The R code will be automatically 
 // run after the compilation.
--- ising_derivative.R
+++ ising_derivative.R
@@ -7,18 +7,70 @@ require(Rcpp)
 
 N = 10
 mu = -0.2
-beta = 0.0
+beta = 0.15
 kT = 1.0
-
-x = matrix(sample(c(-1, 1), N * N, replace = TRUE), nrow = N)
 
 sourceCpp("ising.cpp")
 
-getEdE = function(mu, beta) {
-  S = 100000 - 1
-  out = ising(x, mu, beta, kT, S)
-  out = ising(out$x, mu, beta, kT, S)
-  x = out$x
+S = 100000 - 1
+x = matrix(sample(c(-1, 1), N * N, replace = TRUE), nrow = N)
+out = ising(x, mu, beta, kT, S)
+
+getphi = function(mu, beta, tol = 0.1, chains = 4) {
+  S = 10^4 - 1
+  x = list()
+  states = list()
+
+  for(chain in 1:chains) {
+    xT = matrix(sample(c(-1, 1), N * N, replace = TRUE), nrow = N)
+    out = ising(xT, mu, beta, kT, S) #Warmup
+    out = ising(out$x, mu, beta, kT, S)
+    x[[chain]] = out$x
+    states[[chain]] = as.tibble(out$states) %>% mutate(chain = chain)
+  }
+  
+  for(f in 1:100) {
+    pc = bind_rows(states) %>%
+      group_by(chain) %>%
+      summarize(m = mean(solo),
+                v = var(solo),
+                n = n())
+    
+    #wt = mean(pc %>% pull(v))
+    #bt = var(pc %>% pull(m))
+    
+    #sig2h = (S - 1) * wt / S + bt
+    #nut = 2 * (sig2h + bt / chains)^2 / wt
+    #r = ((sig2h + bt / chains) / wt) * nut / (nut - 2)
+    #print.data.frame(pc)
+    #print(paste(sig2h, wt, bt, f, r))
+    
+    if(sd(pc %>% pull(m)) < tol) {
+      print(f)
+      break;
+    }
+    
+    for(chain in 1:chains) {
+      out = ising(x[[chain]], mu, beta, kT, S)
+      x[[chain]] = out$x
+      states[[chain]] = bind_rows(states[[chain]], as.tibble(out$states) %>% mutate(chain = chain))
+    }
+  }
+  
+  bind_rows(states)
+}
+
+getphi(0.2, 0.5, 1.0) %>%
+  group_by(chain) %>%
+  top_n(10000) %>%
+  mutate(rn = row_number()) %>%
+  gather(key, value, c(solo, pairs)) %>%
+  ggplot(aes(rn, value, color = key)) +
+  geom_line() +
+  facet_grid(. ~ chain)
+
+getEdE = function(mu, beta, r = 1.03) {
+  getphi(mu, beta, r)
   as.tibble(out$states) %>%
     summarize(mean_solo = mean(solo),
               mean_pairs = mean(pairs),
@@ -32,6 +84,14 @@ getEdE = function(mu, beta) {
     rename(mphi = mean_solo) %>%
     mutate(mu = mu)
 }
+
+getEdE(mu, beta)
+
+S = 1000
+x = matrix(sample(c(-1, 1), N * N, replace = TRUE), nrow = N)
+ising(x, mu, beta, kT, S)
+as.tibble(out$states) %>% mutate(rn = row_number()) %>%
+  gather(key, value, c(solo, pairs)) %>% ggplot(aes(rn, value, color = key)) + geom_point()
 
 s1 = getEdE(0.2, 0.5)
 s2 = getEdE(0.2, 0.5 + 0.01)
