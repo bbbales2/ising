946993de436af037ea67f79af9a2791aaaf5dc11
bbbales2 <bbbales2@gmail.com>
2017-09-01 16:32:24 -0700

    Added phi1 penalization

--- stan_gradients_penalize_phi1.R
+++ stan_gradients_penalize_phi1.R
@@ -26,8 +26,11 @@ results = list(mu = seq(-5.0, 5.0, length = 21),
   pmap(function(mu, beta, gamma, seed) {
     ising_gibbs(x, mu, beta, gamma, kT, S, seed)$states %>% as.tibble %>%
       summarize(mphi = mean(solo) / N^2,
+                mphi1 = mean(pairs) / N^2,
                 dmphi_dbeta = -cov(solo, pairs) / kT / N^2,
-                dmphi_dgamma = -cov(solo, corners) / kT / N^2) %>%
+                dmphi1_dbeta = -var(pairs) / kT / N^2,
+                dmphi_dgamma = -cov(solo, corners) / kT / N^2,
+                dmphi1_dgamma = -cov(pairs, corners) / kT / N^2) %>%
       mutate(mu = mu, beta = beta, gamma = gamma, seed = seed, S = S)
   }) %>% bind_rows %>%
   mutate(seed = factor(seed))
@@ -42,32 +45,34 @@ dataS = list(mu = results %>% pull(mu) %>% unique,
   pmap(function(mu, seed) {
     ising_gibbs(x, mu, betaRef, gammaRef, kT, S * 100, seed)$states %>% as.tibble %>%
       summarize(phi = mean(solo) / N^2,
+                phi1 = mean(pairs) / N^2,
                 dmphi_dbeta = -cov(solo, pairs) / kT / N^2,
+                dmphi1_dbeta = -var(pairs) / kT / N^2,
                 dmphi_dgamma = -cov(solo, corners) / kT / N^2,
+                dmphi1_dgamma = -cov(pairs, corners) / kT / N^2,
                 d2mphi_dbeta2 = (mean(solo * pairs^2) +
-                  -mean(solo * pairs) * mean(pairs) +
-                  -cov(solo, pairs) * mean(pairs) +
-                  -var(pairs) * mean(solo)) / N^2,
+                                 -mean(solo * pairs) * mean(pairs) +
+                                 -cov(solo, pairs) * mean(pairs) +
+                                 -var(pairs) * mean(solo)) / N^2,
                 d2mphi_dgamma2 = (mean(solo * corners^2) +
-                  -mean(solo * corners) * mean(corners) +
-                  -cov(solo, corners) * mean(corners) +
-                  -var(corners) * mean(solo)) / N^2,
+                                  -mean(solo * corners) * mean(corners) +
+                                  -cov(solo, corners) * mean(corners) +
+                                  -var(corners) * mean(solo)) / N^2,
                 d2mphi_dbetadgamma = (mean(solo * pairs * corners) +
-                  -mean(solo * pairs) * mean(corners) +
-                  -cov(solo, corners) * mean(pairs) +
-                  -cov(corners, pairs) * mean(solo)) / N^2) %>%
+                                      -mean(solo * pairs) * mean(corners) +
+                                      -cov(solo, corners) * mean(pairs) +
+                                      -cov(corners, pairs) * mean(solo)) / N^2) %>%
       mutate(mu = mu, seed = seed)
   }) %>% bind_rows
 
 data = dataS %>% group_by(mu) %>% summarize_at(vars(everything()), mean) %>% select(-seed)
 
-fit_iid = stan('models/iid.stan', data = list(N = nrow(data), y = data$phi, sigma = 0.1), iter = 10)
-fit_mvn = stan('models/mvn.stan', data = list(N = nrow(data), y = data$phi, x = data$mu, sigma = 0.1, l = 0.5), iter = 10)
+fit_iid = stan('models/iid.stan', data = list(N = nrow(data) + 1, y = c(data$phi, data$phi1[[10]]), sigma = 0.1), iter = 10)
 
 lpres = results %>% group_by(beta, gamma, seed) %>%
-  summarize(nlp = -log_prob(fit_iid, mphi),
-            dnlp_dbeta = -grad_log_prob(fit_iid, mphi) %*% dmphi_dbeta,
-            dnlp_dgamma = -grad_log_prob(fit_iid, mphi) %*% dmphi_dgamma)
+  summarize(nlp = -log_prob(fit_iid, c(mphi, mphi1[[10]])),
+            dnlp_dbeta = -grad_log_prob(fit_iid, c(mphi, mphi1[[10]])) %*% c(dmphi_dbeta, dmphi1_dbeta[[10]]),
+            dnlp_dgamma = -grad_log_prob(fit_iid, c(mphi, mphi1[[10]])) %*% c(dmphi_dgamma, dmphi1_dgamma[[10]]))
 
 mlpres = lpres %>%
   group_by(beta, gamma) %>%
