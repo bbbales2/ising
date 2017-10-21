a021e8e9ad258c379cf8adc38cd129021131b2d7
bbbales2 <bbbales2@gmail.com>
2017-10-21 15:48:52 -0700

    Ran some tests to reproduce custom solver results

--- casm_helper.R
+++ casm_helper.R
@@ -3,7 +3,15 @@ library(tidyverse)
 library(stringr)
 
 runMC = function(path) {
-  system("(cd /home/bbales2/casm/cubic_3d; rm -rf conditions.* results.json; /home/bbales2/local/casm/bin/casm monte -s monte.json)")
+  system(paste0("(cd ",
+                path,
+                "; rm -rf conditions.* results.json; /home/bbales2/local/casm/bin/casm monte -s monte.json)"))
+}
+
+getResults = function(path) {
+  df = fromJSON(paste0(path, "/results.json")) %>% as.tibble
+  colnames(df) = gsub('[<>\\(\\),]', '', colnames(df))
+  df
 }
 
 getCorrs = function(path) {
@@ -13,19 +21,17 @@ getCorrs = function(path) {
 
   dfs = list()  
   for(i in 1:length(files)) {
-    df = fromJSON(paste0(path, files[[i]], "/observations.json.gz")) %>% as.tibble %>%
+    df = fromJSON(paste0(path, "/", files[[i]], "/observations.json.gz")) %>% as.tibble %>%
       select(starts_with("corr"))
     
     colnames(df) = gsub('[\\(\\)]', '', colnames(df))
     colNumbers = order(as.numeric(gsub('^corr([0123456789]*)$', '\\1', colnames(df))))
     dfs[[i]] = df %>% select(noquote(colnames(df)[colNumbers])) %>%
-      mutate(t = i)
+      mutate(t = i, mci = row_number())
   }
   
   dfs %>% bind_rows %>% select(t, everything())
 }
-
-#getResults("/home/bbales2/casm/cubic_3d/")
 
 getECIs = function(path) {
   fpath = paste0(path, "/cluster_expansions/clex.formation_energy/calctype.default/ref.default/bset.default/eci.default/eci.json")
---
+++ casm_test.R
@@ -0,0 +1,45 @@
+source("casm_helper.R")
+source("ising_helpers.R")
+require(Rcpp)
+sourceCpp("likelihoods.cpp")
+
+library(reshape2)
+library(tidyverse)
+library(ggplot2)
+library(rstan)
+library(stats)
+library(parallel)
+
+path = "/home/bbales2/casm/cubic_2d"
+setECIs(path, c(0.0, 0.0, 0.5, 0.0, 0.0))
+runMC(path)
+corrs = getCorrs(path)
+results = getResults(path)
+
+corrs %>% ggplot(aes(mci, corr1)) +
+  geom_point(aes(color = as.factor(t)))
+
+results %>% ggplot(aes(param_chem_pota, corr1)) +
+  geom_point()
+
+out = list(beta = seq(0.0, 1.5, length = 11),
+     gamma = seq(0.0, 1.5, length = 11)) %>%
+  expand.grid %>%
+  as.tibble %>%
+  mutate(output = pmap(., function(beta, gamma) {
+    setECIs(path, c(0.0, 0.0, beta, gamma, 0.0))
+    runMC(path)
+    list(results = getResults(path), corrs = getCorrs(path))
+  })) %>%
+  mutate(results = map(output, ~ .x$results),
+         corrs = map(output, ~ .x$corrs)) %>%
+  select(-output)
+
+out %>%
+  select(-corrs) %>%
+  unnest %>%
+  group_by(beta, gamma) %>%
+  select(param_chem_pota, corr1) %>%
+  ggplot(aes(param_chem_pota, corr1)) +
+  geom_point() +
+  facet_grid(beta ~ gamma, labeller = label_both)
