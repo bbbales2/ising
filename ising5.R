b04c01e4e5a8182513aa16ce7a71222cebd9a927
bbbales2 <bbbales2@gmail.com>
2017-12-24 12:03:36 -0800

    Added more losses

--- ising5.R
+++ ising5.R
@@ -41,6 +41,16 @@ if(!useFuncLoss & !useCorrLoss) {
 sourceCpp("covariance2.cpp")
 source("ising_helpers2.R")
 
+p = function(x) {
+  as.tibble(melt(x)) %>%
+    ggplot(aes(x = Var1, y = Var2, fill = factor(value))) +  
+    geom_tile() +
+    xlab("x") +
+    ylab("y") +
+    coord_equal() +
+    scale_y_reverse()
+}
+
 N = 2 * 3 * 5
 sigma = 0.01
 S = 200
@@ -48,7 +58,6 @@ mus = seq(-5.0, 5.0, length = 21)
 
 x = matrix(sample(c(-1, 1), N * N, replace = TRUE), nrow = N)
 
-# This is the output for the grid of test parameters
 ising_sweep = function(x, beta, S, seeds) {
   list(mu = mus,
        seed = seeds) %>%
@@ -57,45 +66,33 @@ ising_sweep = function(x, beta, S, seeds) {
     (function(df) split(df, 1:nrow(df))) %>%
     mclapply(function(row) {
       res = ising_gibbs_derivs(x, row$mu, beta, S, row$seed)
-      df = res$jac[1,]
-      names(df) = map(names(df), ~ paste0("dX0d", .))
       
-      bind_cols(res$f %>% t %>% as.tibble, df %>% t %>% as.tibble) %>%
-        mutate(mu = row$mu, seed = row$seed, S = S) %>%
-        mutate(!!!beta)
-    }, mc.cores = 24) %>%
-    bind_rows %>%
-    mutate(seed = factor(seed))
+      res$mu = row$mu
+      res$seed = row$seed
+      
+      res
+    }, mc.cores = 24)
 }
 
-K = rbf_cov_vec(mus %>% as.matrix, mus %>% as.matrix, c(1.0))
+data = map(ising_sweep(x, beta, S * 10, 2), ~ list(f = .$f))
 
-data = list(X0 = ising_sweep(x, beta, S * 10, 2) %>% pull(X0),
-            Q = ising_gibbs_derivs(x, 0.0, beta, S * 10, 2)$f[-1, ])
-
-UgradU = function(b) {
+UgradU = function(q) {
   u = 0.0
+  dudq = rep(0, length(q))
   r = sample(10000000, 1)
-  gradv = rep(0, length(b))
-  gradb = rep(0, length(b))
-  dudq = rep(0, length(b))
   
-  if(useFuncLoss) {
-    df = ising_sweep(x, b, S, r)
-    u = u + 0.5 * sum((df$X0 - data$X0)^2)
-    gradv = (df$X0 - data$X0)
-    dudq = dudq + df %>% gather(var, y, starts_with("dX0")) %>%
-      group_by(var) %>%
-      mutate(yh = K %*% fsolve(K + diag(0.25, length(mus)), y %>% as.matrix)) %>%
-      summarize(y = y %*% gradv,
-                yh = yh %*% gradv) %>% pull(yh)
-  }
-  
-  if(useCorrLoss) {
-    dQ = ising_gibbs_derivs(x, 0.0, b, S, r)
-    QQ = dQ$f[2:length(dQ$f), ]
-    u = u + 0.5 * sum((QQ - data$Q)^2)
-    dudq = dudq + (QQ - data$Q) %*% dQ$jac[2:length(dQ$f), ]
+  calc = ising_sweep(x, q, S, r)
+
+  for(i in 1:length(calc)) {
+    if(useFuncLoss) {
+      u = u + 0.5 * (calc[[i]]$f[1] - data[[i]]$f[1])^2
+      dudq = dudq + (calc[[i]]$f[1] - data[[i]]$f[1]) * calc[[i]]$jac[1,]
+    }
+    
+    if(useCorrLoss) {
+      u = u + 0.5 * sum((calc[[i]]$f[-1] - data[[i]]$f[-1])^2)
+      dudq = dudq + (calc[[i]]$f[-1] - data[[i]]$f[-1]) %*% calc[[i]]$jac[-1,]
+    }
   }
   
   list(u = 100 * u,
@@ -106,7 +103,7 @@ fn = function(b) { UgradU(b)$u }
 gn = function(b) { UgradU(b)$dudq }
 
 opts = list()
-for(o in 1:250) {
+for(o in 1:100) {
   b = rnorm(5, 0.1, 0.25)
   names(b) = c("b0", "b1", "b2", "b3", "b4")
   out = optim(b, fn, gn, method = "L-BFGS-B", control = list(maxit = 200, trace = 1, REPORT = 1))
@@ -116,13 +113,8 @@ for(o in 1:250) {
   cat("finished optimization", o, "\n")
 }
 
-opt_samples = map(1:length(opts), ~ bind_cols(list(x = mus,
-                                                   which = "opt",
-                                                   opt = .,
-                                                   lp = opts[[.]] %>% pull(lp) %>% max) %>% as.tibble,
-                                              ising_sweep(x, opts[[.]] %>% top_n(1) %>%
-                                                            gather(name, b, 1:5) %>% pull(b) %>%
-                                                            setNames(names(b)), S, sample(10000000, 1)))) %>%
-  bind_rows()
+opt_samples = map(1:length(opts),
+                  ~ ising_sweep(x, opts[[.]] %>% gather(name, b, 1:5) %>% pull(b) %>%
+                                                 setNames(names(b)), S, sample(10000000, 1)))
 
 save.image(args$output)
