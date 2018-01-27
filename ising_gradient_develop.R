b348398433f905e58b09d220cd5ed29c04e01d7f
bbbales2 <bbbales2@gmail.com>
2018-01-27 10:52:42 -0800

    More experiments!

--- ising_gradient_develop.R
+++ ising_gradient_develop.R
@@ -56,15 +56,15 @@ smu = function(mu, beta, gamma) {
   list(g = t(res[,keep]) / NN, Eg = t(t(f[keep, 1])), Egrad = jac[keep,])
 }
 
-#GgradG = function(g) {
+GgradG = function(g) {
   beta = g[1:5]
   gamma = g[6]
 
-  a = mclapply(mus[1:2], function(mu) { smu(mu, beta, gamma) }, mc.cores = min(length(mus), 24))
+  a = mclapply(mus, function(mu) { smu(mu, beta, gamma) }, mc.cores = min(length(mus), 24))
 
-  for(i in 1:length(data)) {
-    S = length(data[[i]]$f)
-    for(j in 1:S) {
+  for(i in 1:length(a)) {
+    J = length(a[[i]]$Eg)
+    for(j in 1:J) {
       a[[i]]$g[j,] = a[[i]]$g[j,] - data[[i]]$f[j]
       a[[i]]$Eg[j] = a[[i]]$Eg[j] - data[[i]]$f[j]
     }
@@ -73,100 +73,34 @@ smu = function(mu, beta, gamma) {
   list(g = do.call("rbind", map(a, ~ .$g)),
        Eg = do.call("rbind", map(a, ~ .$Eg)),
        Egrad = do.call("rbind", map(a, ~ .$Egrad)))
-#}
-
-#%>% do.call("rbind", .)
-
-ising_sweep = function(x, q, S, seeds) {
-  list(mu = mus,
-       seed = seeds) %>%
-    expand.grid %>%
-    as.tibble %>%
-    (function(df) split(df, 1:nrow(df))) %>%
-    mclapply(function(row) {
-      beta = q[1:5]
-      gamma = q[6]
-      res = ising_gibbs_derivs(x, row$mu, beta, gamma, S, row$seed)
-      
-      res$mu = row$mu
-      res$seed = row$seed
-      
-      res
-    }, mc.cores = 24)
 }
 
-data = map(ising_sweep(x, c(beta, gamma), S * 10, 2), ~ list(f = .$f))
-
-UgradU = function(q) {
-  u = 0.0
-  dudq = rep(0, length(q))
-  r = sample(10000000, 1)
-  
-  calc = ising_sweep(x, q, S, r)
-  
-  for(i in 1:length(calc)) {
-    if(useFuncLoss) {
-      u = u + 0.5 * (calc[[i]]$f[1] - data[[i]]$f[1])^2
-      dudq = dudq + (calc[[i]]$f[1] - data[[i]]$f[1]) * calc[[i]]$jac[1,]
-    }
-    
-    if(useCorrLoss > 0) {
-      u = u + 0.5 * sum((calc[[i]]$f[2:(1 + useCorrLoss)] - data[[i]]$f[2:(1 + useCorrLoss)])^2)
-      dudq = dudq + (calc[[i]]$f[2:(1 + useCorrLoss)] - data[[i]]$f[2:(1 + useCorrLoss)]) %*% calc[[i]]$jac[2:(1 + useCorrLoss),]
-    }
+getOmega = function(g) {
+  omega = matrix(0, nrow = nrow(g$g), nrow(g$g))
+  for(i in 1:ncol(g$g)) {
+    omega = omega + g$g[,i] %*% t(g$g[,i])
   }
-  
-  list(u = 100 * u,
-       dudq = 100 * dudq)
+  omega / ncol(g$g) + 1e-6 * diag(nrow(omega))
 }
 
-GgradG = function(q) {
-  r = sample(10000000, 1)
-  
-  calc = ising_sweep(x, q, S, r)
-  
-  g = matrix(0, nrow = length(calc) * (1 + useCorrLoss), ncol = 1)
-  dgdq = matrix(0, nrow = length(calc) * (1 + useCorrLoss), ncol = length(q))
-  ii = 1
-  for(i in 1:length(calc)) {
-    for(j in 1:(1 + useCorrLoss)) {
-      g[ii] = 0.5 * (calc[[i]]$f[j] - data[[i]]$f[j])^2
-      dgdq[ii,] = (calc[[i]]$f[j] - data[[i]]$f[j]) * calc[[i]]$jac[j,]
-      ii = ii + 1
-    }
-  }
-  
-  list(g = g,
-       dgdq = dgdq)
-}
-
-fn = function(b) { UgradU(b)$u }
-gn = function(b) { UgradU(b)$dudq }
-
-b = rnorm(6, 0.1, 0.25)
-eps = 10.0
-for(i in 1:100) {
-  ugradu = UgradU(b)
-  grad = ugradu$dudq
-  
-  cat("lp : ", ugradu$u, ", params : ", b, "\n")
-  b = b - eps * grad / (sum(grad^2) + 1e-10)
-}
-cat("lp : ", ugradu$u, ", params : ", b, "\n")
+#getOmega(g)
 
 opts = list()
-for(o in 1:400) {
+for(j in 1:10) {
   b = rnorm(6, 0.1, 0.25)
-  names(b) = c("b0", "b1", "b2", "b3", "b4", "b5")
-  out = optim(b, fn, gn, method = "L-BFGS-B", control = list(maxit = 200, trace = 1, REPORT = 1))
-  opts[[o]] = out$par %>% as.list %>% as.tibble %>%
-    mutate(lp = -out$value, which_opt = o)
+  eps = 0.1
+  M = 500
+  scaling = -log(0.01) / M
+  for(i in 1:M) {
+    g = GgradG(b)
+    W = diag(nrow(g$g))
+    u = t(g$Eg) %*% solve(W, g$Eg)
+    grad = t(g$Eg) %*% solve(W, g$Egrad)
+    dt = eps * exp(-i * scaling)
+    
+    cat("j : ", j, ", dt : ", dt, ", lp : ", u, ", params : ", b, "\n")
+    b = b - dt * grad / (sqrt(sum(grad^2)) + 1e-10)
+  }
   
-  cat("finished optimization", o, "\n")
+  opts[j] = b
 }
-
-opt_samples = map(1:length(opts),
-                  ~ ising_sweep(x, opts[[.]] %>% gather(name, b, 1:6) %>% pull(b) %>%
-                                  setNames(names(b)), S, sample(10000000, 1)))
-
-save.image(args$output)
